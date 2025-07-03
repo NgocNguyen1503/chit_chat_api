abc8f97e43629c65d57b35f9ec1e6c718bd6c735
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-03 23:35:57 +0700

    Fix missing order by in list message & format messages time

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\ChatRoom;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -45,16 +46,17 @@ class UserController extends Controller
         $chatroom = ChatRoom::select(['id', 'thumbnail', 'users_id'])
             ->where('id', $params['chatroom_id'])
             ->with([
-                'messages' => fn($q) => $q->select(['messages.user_id', 'messages.chatroom_id', 'messages.message', 'messages.created_at', 'users.id as user_id', 'users.name as user_name'])
-                    ->join('users', 'messages.user_id', '=', 'users.id')
+                'messages' => fn($q) => $q->select(['messages.id', 'messages.user_id', 'messages.chatroom_id', 'messages.message', 'messages.created_at', 'users.id as user_id', 'users.name as user_name'])
+                    ->join('users', 'messages.user_id', '=', 'users.id')->orderBy('created_at', 'asc')
             ])->first();
         $chatroom->chatroom_name = User::select(['id', 'name'])
             ->whereIn('id', explode(",", $chatroom->users_id))
             ->get()->pluck('name')->implode(', ');
         collect(explode(',', $chatroom->users_id))->count() > 2 ? $chatroom->chatroom_type = 'group' : $chatroom->chatroom_type = 'friend';
         foreach ($chatroom->messages as $message) {
+            $message->_created_at = Carbon::parse($message->created_at)->format('h:i A');
             $message->user_id != Auth::user()->id ? $message->type = 'friend' : $message->type = 'me';
-            unset($message->user_id, $message->chatroom_id);
+            unset($message->user_id, $message->chatroom_id, $message->created_at);
         }
         unset($chatroom->users_id, $chatroom->id);
         return ApiResponse::success($chatroom);
